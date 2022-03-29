3f402d43247465fa3e3048daf4bfdca6665937ea
jsebas2220 <67612479+jsebas2220@users.noreply.github.com>
2022-03-29 10:34:07 -0500

    Add files via upload

---
+++ RetoTecnico/README.md
@@ -0,0 +1,9 @@
+Reto técnico training league Full Stack
+
+-- En el siguiente reto se encuntra una evolución sobre los conocmientos basicos que he obtenido a lo largo de los estudios que obtenido en progrmaación, no son muy solidos, y mas bien basicos, consultadno y viendo otros casos, logre contruir algo, tal vez no sea igual al reto, pero creo que algunos puntos los logre al menos plantear, claro esta mis conocimientos son muy basicos y debido a eso no logre terminarlo todo iaugl al reto.
+
+espero sirva de algo
+
+url de publicación. https://retotecnicosoftka.netlify.app/
+
+Git https://github.com/jsebas2220/PruebaSoftka
---
+++ RetoTecnico/data/data.js
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    question: "Cual de las siguientes opciones no es un editor de codigo?",
+    choices: ["vim", "vscode", "emacs", "word"],
+    answer: "word",
+  },
+  {
+    question: "Que lenguaje no es orientado a Objetos",
+    choices: ["Java", "Haskell", "C++", "Python"],
+    answer: "Haskell",
+  },
+  {
+    question: "Que lenguaje no sirve para estilizar sitios web?",
+    choices: ["Stylus", "Less", "Sass", "CSS", 'PostCSS', 'NextCSS'],
+    answer: "NextCSS",
+  },
+  {
+    question: "Cual es una base de datos No-relacional?",
+    choices: ["Sql", "Mysql", "Acces", "Postgress", 'MongoDB', 'MariaDb'],
+    answer: "MongoDB",
+  },
+  {
+    question: "Cual es un lenguaje compilado?",
+    choices: ["C++", "Python", "Javascript", "React", 'HTML', 'Angular'],
+    answer: "C++",
+  },
+  {
+    question: "Cual no es una ditrubución de linux?",
+    choices: ["Ubuntu", "CentOS", "Red-Hat", "Fedora", 'Debian', 'Windows'],
+    answer: "Windows",
+  },
+  {
+    question: "Que es Git?",
+    choices: ["S.O", "Bases de datos", "Editor de codigo", "Control de versiones", 'Lenguaje compilado', 'Lenguaje interpretado'],
+    answer: "Control de versiones",
+  },
+  {
+    question: "Cual es la ultima version e JavaScript?",
+    choices: ["1.6", "2.0", "ES6", "E4", 'Script5', 'Java200'],
+    answer: "ES6",
+  },
+  {
+    question: "Es un ciclo de programacion",
+    choices: ["Quiza", "Maybe", "While", "Where", 'From', 'Select'],
+    answer: "While",
+  },
+  {
+    question: "Siglas de Documetn Object Model?",
+    choices: ["DOC", "DOM", "Modeule", "ODM", 'Testing', 'QA'],
+    answer: "DOM",
+  },
+];
